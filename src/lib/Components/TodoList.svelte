0a3ed1dddeb6d22ab42139dc6e11d82b422251a3
Pouria Yousefi <pouriayousefid@gmail.com>
2026-02-17 20:14:56 +0330

    delete two same class (bg-color)

--- src/lib/Components/TodoList.svelte
+++ src/lib/Components/TodoList.svelte
@@ -10,7 +10,7 @@
 	{#each todos as todo (todo.id)}
 		<div
 			transition:slide
-			class="todo w-full h-full border border-[#3A3F4D] rounded-2xl bg-[#262A36] flex items-center p-5"
+			class="todo w-full h-full border border-[#3A3F4D]/30 rounded-2xl flex items-center p-5"
 		>
 			<input
 				data-index={todo.id}
